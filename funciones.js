2781b5dd5546b8170f1b765e931986b6e5082392
sebasR26 <sebasR26@gmail.com>
2023-10-30 20:02:27 -0500

    first commit

---
+++ funciones.js
@@ -0,0 +1,26 @@
+//variables golbales
+let imgNivel1 = [{
+    nombre: "all might",
+    url: "img/all.jpeg"
+},
+{
+    nombre: "panda",
+    url: "img/panda.jpg"
+},
+{
+    nombre: "makima",
+    url: "img/makima.webp"
+},
+{
+    nombre: "gojo",
+    url: "img/gojo.jpg_large"
+},
+{
+    nombre: "reze",
+    url: "img/reze.avif"
+},
+{
+    nombre: "sukuna",
+    url: "img/sukuna.jpg"
+}
+]
