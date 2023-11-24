c7903a2c6d7b1bba3fcffd67543c72e972f0ad03
sebasR26 <sebassrp47@gmail.com>
2023-11-24 18:45:43 -0500

    falta sistema de puntuacion

--- funciones.js
+++ funciones.js
@@ -117,6 +117,89 @@ let imgNivel2 = [{
 }
 ]
 
+let imgNivel3 = [{
+    nombre: "all might",
+    url: "img/all.jpeg"
+},
+{
+    nombre: "panda",
+    url: "img/rem.jpg"
+},
+
+{
+    nombre: "makima",
+    url: "img/makima.webp"
+},
+{
+    nombre: "gojo",
+    url: "img/gojo.jpg_large"
+},
+{
+    nombre: "reze",
+    url: "img/reze1.jpg"
+},
+{
+    nombre: "sukuna",
+    url: "img/sukuna.jpg"
+},
+{
+    nombre: "all might",
+    url: "img/all.jpeg"
+},
+{
+    nombre: "panda",
+    url: "img/rem.jpg"
+},
+{
+    nombre: "makima",
+    url: "img/makima.webp"
+},
+{
+    nombre: "gojo",
+    url: "img/gojo.jpg_large"
+},
+{
+    nombre: "reze",
+    url: "img/reze1.jpg"
+},
+{
+    nombre: "sukuna",
+    url: "img/sukuna.jpg"
+},
+{
+    nombre: "gohan",
+    url: "img/gohan.jpg"
+},
+{
+    nombre: "baki",
+    url: "img/baki.jpeg"
+},
+{
+    nombre: "gohan",
+    url: "img/gohan.jpg"
+},
+{
+    nombre: "baki",
+    url: "img/baki.jpeg"
+},
+{
+    nombre: "jack",
+    url: "img/jack.webp"
+},
+{
+    nombre: "zeus",
+    url: "img/zeus.jpg"
+},
+{
+    nombre: "jack",
+    url: "img/jack.webp"
+},
+{
+    nombre: "zeus",
+    url: "img/zeus.jpg"
+}
+]
+
 tablero = document.querySelector(".tablero")
 
 let imagenNombre = [];
@@ -133,25 +216,32 @@ let tiempoTranscurrido;
 let btn_iniciar = document.querySelector(".btn-iniciar")
 let mostrarNivel = document.querySelector(".Nivel")
 let imagenNivel;
+let estoyJugando = false;
+let selecSonido = new Audio("./sonidos/escoger.mp3");
+let adivinarSonido = new Audio("./sonidos/adivinar.mp3");
+let falloSonido = new Audio("./sonidos/fallo.mp3");
+let nivelSonido = new Audio("./sonidos/level up.mp3");
+let perderSonido = new Audio("./sonidos/perdio.mp3");
+
 
 //agregar el evento para iniciar el juego
 
 btn_iniciar.addEventListener("click", function(){
-    tiempoTranscurrido = setInterval(()=>{
-        tiempo--;
-        mostrarTiempo.textContent = tiempo
-        if(tiempo == 10){
-            mostrarTiempo.classList.add("fast")
+    //comprobar que el juego este activo
+    if(estoyJugando == false && nivel==1){
+        estoyJugando = true;
+        nivel1();
+        
+        
+    }else if(estoyJugando == false && nivel==2){
+        estoyJugando = true;
+        nivel2();
+    }else if(estoyJugando == false && nivel==3){
+        estoyJugando = true;
+        nivel3();
+    }
 
-        }else if(tiempo == 0){
-            alert("Perdiste. FIN DEL TIEMPO")
-            clearInterval(tiempoTranscurrido);
-            location.reload();
-        }
-    }, 1000);
-
-    agregarImagen();
-    mostrarNivel.textContent = nivel;
+    
 
 })
 
@@ -173,7 +263,14 @@ function agregarImagen() {
         imagenNivel = imgNivel1
     }else if(nivel == 2){
         imagenNivel = imgNivel2
+    }else if(nivel == 3){
+        imagenNivel = imgNivel3
     }
+
+
+    //colocar imagenes aleatorias
+
+    imagenNivel.sort(()=> Math.random() -0.5);
 
 
     imagenNivel.forEach((imagen, i) => {
@@ -192,6 +289,7 @@ function agregarImagen() {
 //funcion para mostrar las imagenes ocultas
 
 function mostrarImg() {
+    selecSonido.play();
     let ImgID = this.getAttribute("id");
     // alert("# de imagen: " + ImgID)
     this.src = imagenNivel[ImgID].url; //modificar url de la imagen
@@ -209,7 +307,8 @@ function compararImg() {
     let imagenesTablero = document.querySelectorAll(".tablero > div > img");
     if (imagenNombre[0] == imagenNombre[1]) {
         if (imagenId[0] != imagenId[1]) {
-            alert("felicidades adivinaste una imagen")
+            adivinarSonido.play();
+            
             imagenesTablero[imagenId[0]].src = "img/gano.png"
             imagenesTablero[imagenId[1]].src = "img/gano.png"
             imagenesTablero[imagenId[0]].removeEventListener("click", mostrarImg)
@@ -227,7 +326,8 @@ function compararImg() {
 
 
     } else {
-        alert("Mala suerte")
+        falloSonido.play();
+        
         imagenesTablero[imagenId[0]].src = "img/panda.jpg"
         imagenesTablero[imagenId[1]].src = "img/panda.jpg"
         intentos++
@@ -239,6 +339,7 @@ function compararImg() {
 
     //comprobar los aciertos esten completados
     if(nivel == 1 && aciertos == 6){
+        nivelSonido.play();
         tablero.innerHTML = "";
         nivel ++
         mostrarNivel.textContent = nivel;
@@ -250,9 +351,12 @@ function compararImg() {
         tiempo = 50;
         mostrarTiempo.textContent = tiempo;
         alert("ganaste")
+        estoyJugando = false;
 
         
     }else if(nivel == 2 && aciertos==8){
+        nivelSonido.play();
+
         tablero.innerHTML = "";
     nivel ++
     mostrarNivel.textContent = nivel;
@@ -264,6 +368,62 @@ function compararImg() {
     tiempo = 40;
     mostrarTiempo.textContent = tiempo;
     alert("ganaste")
+    estoyJugando = false;
+    }else if(nivel == 3 && aciertos==10){
+        nivelSonido.play();
+        alert("GANASTE EL JUEGO");
+        location.reload();
     }
 }
+
+
+
+function nivel1(){
+    //agregar las imagenes al tablero
+    agregarImagen();
+    mostrarNivel.textContent = nivel;
+
+    //llamar el tiempo
+    tiempoJuego();
+}
+
+function nivel2(){
+    //agregar las imagenes al tablero
+    agregarImagen();
+    
+
+    //llamar el tiempo
+    tiempoJuego();
+}
+
+function nivel3(){
+    //agregar las imagenes al tablero
+    agregarImagen();
+    
+
+    //llamar el tiempo
+    tiempoJuego();
+}
+
+
+function tiempoJuego(){
+    tiempoTranscurrido = setInterval(()=>{
+        tiempo--;
+        mostrarTiempo.textContent = tiempo
+        if(tiempo == 10){
+            mostrarTiempo.classList.add("fast")
+
+        }else if(tiempo == 0){
+            perderSonido.play();
+
+            alert("Perdiste. FIN DEL TIEMPO")
+            
+            clearInterval(tiempoTranscurrido);
+            location.reload();
+        }
+    }, 1000);
+
+    
+}
+
 
